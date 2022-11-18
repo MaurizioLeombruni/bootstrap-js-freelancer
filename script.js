1d700a71dc3d4d15d712d7595f649583ff6386e9
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2022-11-18 13:29:07 +0100

    Aggiunta funzione controllo degli sconti.

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 let price = 0;
+let discount = 1;
+let allowedCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+function checkDiscount(){
+
+    let codeToCheck = document.getElementById('inputDiscount').value;
+
+    for(i=0; i<allowedCodes.length; i++){
+
+        if (codeToCheck == allowedCodes[i]){
+            document.getElementById('inputDiscount').style.color="green";
+            allowedCodes.splice(i, 1);
+            discount = 0.75;
+
+            console.log(allowedCodes);
+            return true;
+        }
+    }
+
+    //document.getElementById('inputDiscount').classList.add("text-danger");
+    document.getElementById('inputDiscount').style.color="red";
+    console.log("Discount code invalid or not found");
+    return false;
+}
 
 function getPrices(){
     let numHours = document.getElementById('inputHours').value;
@@ -12,20 +36,22 @@ function getPrices(){
         return false;
     }
 
+    checkDiscount();
+
     switch(workType){
 
         case 1:
-            price = numHours * 20.5;
+            price = numHours * 20.5  * discount;
             console.log("Price is " + price.toFixed(2));
             break;
 
         case 2:
-            price = numHours * 15.5;
+            price = numHours * 15.5 * discount; 
             console.log("Price is " + price.toFixed(2));
             break;
 
         case 3:
-            price = numHours * 30.5;
+            price = numHours * 30.5 * discount;
             console.log("Price is " + price.toFixed(2));
             break;
         
